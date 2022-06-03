105354bbaaf526e798e9163331790f9e74264331
fernando <fernandoreal.valentim@gmail.com>
2022-06-03 02:02:38 -0300

    Fix: cellhpone design

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-
+let titleH1 = document.getElementById("title").innerText;
 let chkBike = document.getElementById("check"); //checkbox
 //get all "errors" P, then run trough the node list turning each of them invisible;
 function check() {
@@ -12,6 +12,7 @@ function check() {
 
 
    if (chkBike.checked) {
+     document.getElementById("first").style.display = "none";
      document.getElementById("last").style.display = "none";
      document.getElementById("inputs").style.display = "none";
      document.getElementById("returnPage").style.display = "flex";
@@ -20,6 +21,7 @@ function check() {
 
 //return to main page
 function returnButton() {
+  document.getElementById("first").style.display = "block";
   document.getElementById("last").style.display = "flex";
   document.getElementById("inputs").style.display = "flex";
   document.getElementById("returnPage").style.display = "none";
