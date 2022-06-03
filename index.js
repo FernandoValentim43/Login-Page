9bd05a91ba61543df14295bd4e75242214dab898
fernando <fernandoreal.valentim@gmail.com>
2022-06-02 21:56:44 -0300

    Feat: checkbox js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+//get all elements with "error" class as a nodelist,
+//then go trough each element of the list changing its visibility
+var chkBike = document.getElementById("check");
+
+
 function check() {
    let errors = document.getElementsByClassName('error');
    for (let i = 0 ; i < errors.length ; i ++) {
     var s = errors[i].style;
     s.visibility = "visible";
    }
-}
 
-//get all elements with "error" class as a nodelist, them go trough each element of the list changing its visibility
+
+   if (chkBike.checked) {
+      
+  } 
+};
+
+
+
+
